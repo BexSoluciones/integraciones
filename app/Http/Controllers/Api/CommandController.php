0f4998e6b052c23fe6a70c9149e175dbe4c86d0c
Jhon-Marulanda <jhonmarulanda54@hotmail.com>
2023-11-14 16:42:13 -0500

    Apis para diferentes consumos de servicios

--- app/Http/Controllers/Api/CommandController.php
+++ app/Http/Controllers/Api/CommandController.php
@@ -18,24 +18,34 @@ class CommandController extends Controller
             set_time_limit(0);
             Artisan::call('command:update-information '.$request->name_db);
             $output = Artisan::output();
-
-            return $output;
-            
-            /*
-            if ($output === 0) {
-                // El comando se ejecutó con éxito
-                Artisan::call('command:export-information');
-                return 'exito';
-            } else {
-                // El comando falló
-                $output = Artisan::output();
-                echo $output;
-                return 'error';
-            }*/
+            if($output){
+                Artisan::call('command:export-information '.$request->name_db);
+            }
+            $output = Artisan::output();
+            if(empty($output)){
+                return response()->json(['status' => 200, 'response' => 'Ha ocurrido un arror al procesar la peticion']);    
+            }
+            return response()->json(['status' => 200, 'response' => 'Proceso finalizado']);
         } catch (\Exception $e) {
             Log::error('Error schedule: ' . $e->getMessage());
             return 'error';
         }
-        
+    }
+
+    public function uploadOrder(Request $request){
+        try{
+            set_time_limit(0);
+            Artisan::call('command:upload-order', [
+                'database' => $request->name_db,
+                'area' => $request->area,
+                'closing' => $request->closing,
+            ]);
+            $output = Artisan::output();
+
+            return response()->json(['status' => 200, 'response' => $output]);
+        } catch (\Exception $e) {
+            Log::error('Error uploadOrder: ' . $e->getMessage());
+            return  'Error uploadOrder: ' . $e->getMessage();
+        }
     }
 }
