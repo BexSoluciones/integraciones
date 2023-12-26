d5eeef9344c2c85c0031d5e8b24ea09f353f88f1
Jhon-Marulanda <jhonmarulanda54otmail.com>
2023-12-26 09:12:05 -0500

    Actualizacion response errores

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,13 +28,15 @@ class Kernel extends ConsoleKernel
                 // Ejecuta el comando
                 $schedule->command($parameter->command, [$parameter->name_db, '0', $parameter->id, 1])
                 ->before(function () use ($parameter) {
+                    Log::info('paso 1');
                     // Se cambia el state a 2 para saber que se esta ejecutando
                     $parameter->updateOrInsert(['name_db' => $parameter->name_db], ['state' => '2']);
+                    Log::info('paso 2');
                 })
                 // Si todo sale bien ejecuta el siguiente comando
                 ->cron($parameter->cron_expression)
-                ->onSuccess(function (Stringable $output) use ($parameter) {
-                    Log::info('llego');
+                ->after(function (Stringable $output) use ($parameter) {
+                    Log::info('paso 3');
                     // Llamar a otro comando si es necesario
                     Artisan::call('command:export-information', [
                         'tenantDB'         => $parameter->name_db,
@@ -43,27 +45,32 @@ class Kernel extends ConsoleKernel
                         'id_importation'   => $parameter->id,
                         'type'             => 1
                     ]);
-
+                    Log::info('paso 4');
                     //Si finaliza correctamente se cambio a state 1 para que pueda volver a ejecutarse
                     $parameter->updateOrInsert(['name_db' => $parameter->name_db], ['state' => '1']);
-                  
+                    Log::info('paso 5');
                     $importationInCurse = Importation_Demand::importationInCurse($parameter->name_db, $parameter->area)->first();
                     if(isset($importationInCurse)){
                         Importation_Demand::updateOrInsert(
                             ['consecutive' => $importationInCurse->consecutive], ['state' => 3, 'updated_at' => $currentTime]
                         );
                     }
+                    Log::info('paso 6');
                 })
+                /*
                 //si ocurre un error se se guarda y se cambia astate 1 para que vuelva aquedar activo 
                 ->onFailure(function (Stringable $output) use ($parameter) {
+                    Log::info('paso 7');
+                    Log::info($output);
                     $parameter->updateOrInsert(['name_db' => $parameter->name_db], ['state' => '1']);
+                    /*
                     Tbl_Log::create([
                         'id_importation' => $parameter->id,
                         'type'           => 1,
                         'descripcion'    => 'Kernel[schedule()] => '.$output
                     ]);
-                    return;
-                })
+                    Log::info('paso 8');
+                })*/
                 // Sirve para que un schedule no se ejecute encima de otro y espere 10 mn
                 ->withoutOverlapping(10);
             }
