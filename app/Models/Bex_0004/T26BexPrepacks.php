a07a302da45c6fe1fed5a8af579b3d104c01e8d1
Jhon-Marulanda <jhonmarulanda54otmail.com>
2024-01-04 14:07:55 -0500

    Montaje Bex_004

---
+++ app/Models/Bex_0004/T26BexPrepacks.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Bex_0004;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class T26BexPrepacks extends Model
+{
+    use HasFactory;
+
+protected $connection = 'dynamic_connection';
+protected $table = 't26_bex_prepacks';
+protected $fillable = ['codprepack', 'codproducto', 'cajas', 'unidades', 'nomprepack', 'estado'];
+public $timestamps = false;
+}
