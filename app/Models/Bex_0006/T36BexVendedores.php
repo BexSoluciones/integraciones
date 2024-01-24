26e227c75865e40831a4b0ccaba28a40e4a3d343
johnftv85 <j.fredytv@gmail.com>
2024-01-24 08:18:52 -0500

    import/bexmovil

--- app/Models/Bex_0006/T36BexVendedores.php
+++ app/Models/Bex_0006/T36BexVendedores.php
@@ -4,7 +4,7 @@ namespace App\Models\Bex_0006;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class T36BexVendedores extends Model
 {
     use HasFactory;
@@ -13,4 +13,10 @@ protected $connection = 'dynamic_connection';
 protected $table = 't36_bex_vendedores';
 protected $fillable = ['compania', 'tercvendedor', 'nomvendedor', 'coddescuento', 'codportafolio', 'codsupervisor', 'nomsupervisor', 'nitvendedor', 'centroop', 'bodega', 'tipodoc', 'cargue', 'estado', 'estadosuperv', 'codvendedor'];
 public $timestamps = false;
+
+public function scopeInsertDataToTblmsupervisor($query){
+    return $query->select('codsupervisor', DB::raw('CONCAT("SUPERVISOR ", codsupervisor) as nomsupervisor'))
+    ->where('estadosuperv', 'A')
+    ->groupBy('codsupervisor');
+}
 }
