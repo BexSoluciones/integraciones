a85cf5711c47211c0c27580a3092ea812be64676
Heider Zapa <heider.zapa@bexsoluciones.com>
2024-03-12 18:15:19 -0500

    fix

--- app/Http/Controllers/Api/CommandController.php
+++ app/Http/Controllers/Api/CommandController.php
@@ -24,6 +24,9 @@ class CommandController extends Controller
 
             $data = $request->timeValidator();
 
+
+            return $data;
+
             $importation = Importation_Demand::create([
                 'command' => 'command:update-information',
                 'name_db' => $request->name_db,
