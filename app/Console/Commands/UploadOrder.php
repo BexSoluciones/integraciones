729c96cd5f6ab4182780540e2946eda5bba204f7
Heider Zapa <heider.zapa@bexsoluciones.com>
2024-03-12 20:04:44 -0500

    s

--- app/Console/Commands/UploadOrder.php
+++ app/Console/Commands/UploadOrder.php
@@ -30,6 +30,11 @@ class UploadOrder extends Command
             $type           = $this->argument('type', null);
         
             $configDB = $this->connectionDB($db, 'externa', $area); 
+
+            print_r($configDB);
+                echo "holiwis";
+    
+                exit;
        
             if($configDB != 0){
                 DB::connection('mysql')->table('tbl_log')->insert([
