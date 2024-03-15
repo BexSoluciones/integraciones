62f2b0ae56ce267d62e61c0e7ec5b623f9168d27
Heider Zapa <heider.zapa@bexsoluciones.com>
2024-03-15 12:05:34 -0500

    add

--- app/Console/Commands/ExportInformation.php
+++ app/Console/Commands/ExportInformation.php
@@ -86,7 +86,7 @@ class ExportInformation extends Command
 
                 foreach ($customMethods as $method) {
                     $methodName = $method->method;
-                    $this->insertLog(null, null, $methodName);
+                    $this->info($methodName ?: "error");
                     if ($tableName == $method->name_table) {
                         $this->performCustomInsert($custom, $methodName, $conectionBex, $conectionSys, $datosAInsertar, $id_importation, $type, $modelInstance, $tableName);
                         $this->info("◘ Proceso $methodName Finalizado");
